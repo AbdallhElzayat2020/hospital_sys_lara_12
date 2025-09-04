5084e43b14660f302aa0b7556f478aab5ceb7e3f
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-09-04 08:31:25 +0300

    add admin authentication and update dashboard layout; create login and signin views

--- resources/views/dashboard/pages/home.blade.php
+++ resources/views/dashboard/pages/home.blade.php
@@ -1,10 +1,5 @@
 @extends('layouts.master')
-@section('css')
-    <!--  Owl-carousel css-->
-    <link href="{{ asset('assets/dashboard/plugins/owl-carousel/owl.carousel.css') }}" rel="stylesheet" />
-    <!-- Maps css -->
-    <link href="{{ asset('assets/dashboard/plugins/jqvmap/jqvmap.min.css') }}" rel="stylesheet">
-@endsection
+@section('title', 'Home')
 @section('page-header')
     <!-- breadcrumb -->
     <div class="breadcrumb-header justify-content-between">
@@ -186,7 +181,7 @@
                         <div class="list-group-item list-group-item-action" href="#">
                             <div class="media mt-0">
                                 <img class="avatar-lg rounded-circle ml-3 my-auto"
-                                    src="{{ asset('assets/dashboard/img/faces/3.jpg') }}" alt="Image description">
+                                     src="{{ asset('assets/dashboard/img/faces/3.jpg') }}" alt="Image description">
                                 <div class="media-body">
                                     <div class="d-flex align-items-center">
                                         <div class="mt-0">
@@ -204,7 +199,7 @@
                         <div class="list-group-item list-group-item-action" href="#">
                             <div class="media mt-0">
                                 <img class="avatar-lg rounded-circle ml-3 my-auto"
-                                    src="{{ asset('assets/dashboard/img/faces/11.jpg') }}" alt="Image description">
+                                     src="{{ asset('assets/dashboard/img/faces/11.jpg') }}" alt="Image description">
                                 <div class="media-body">
                                     <div class="d-flex align-items-center">
                                         <div class="mt-1">
@@ -222,7 +217,7 @@
                         <div class="list-group-item list-group-item-action" href="#">
                             <div class="media mt-0">
                                 <img class="avatar-lg rounded-circle ml-3 my-auto"
-                                    src="{{ asset('assets/dashboard/img/faces/17.jpg') }}" alt="Image description">
+                                     src="{{ asset('assets/dashboard/img/faces/17.jpg') }}" alt="Image description">
                                 <div class="media-body">
                                     <div class="d-flex align-items-center">
                                         <div class="mt-1">
@@ -240,7 +235,7 @@
                         <div class="list-group-item list-group-item-action" href="#">
                             <div class="media mt-0">
                                 <img class="avatar-lg rounded-circle ml-3 my-auto"
-                                    src="{{ asset('assets/dashboard/img/faces/15.jpg') }}" alt="Image description">
+                                     src="{{ asset('assets/dashboard/img/faces/15.jpg') }}" alt="Image description">
                                 <div class="media-body">
                                     <div class="d-flex align-items-center">
                                         <div class="mt-1">
@@ -258,7 +253,7 @@
                         <div class="list-group-item list-group-item-action br-br-7 br-bl-7" href="#">
                             <div class="media mt-0">
                                 <img class="avatar-lg rounded-circle ml-3 my-auto"
-                                    src="{{ asset('assets/dashboard/img/faces/6.jpg') }}" alt="Image description">
+                                     src="{{ asset('assets/dashboard/img/faces/6.jpg') }}" alt="Image description">
                                 <div class="media-body">
                                     <div class="d-flex align-items-center">
                                         <div class="mt-1">
@@ -286,35 +281,35 @@
                 </div>
                 <div class="product-timeline card-body pt-2 mt-1">
                     <ul class="timeline-1 mb-0">
-                        <li class="mt-0"> <i class="ti-pie-chart bg-primary-gradient text-white product-icon"></i> <span
+                        <li class="mt-0"><i class="ti-pie-chart bg-primary-gradient text-white product-icon"></i> <span
                                 class="font-weight-semibold mb-4 tx-14 ">Total Products</span> <a href="#"
-                                class="float-left tx-11 text-muted">3 days ago</a>
+                                                                                                  class="float-left tx-11 text-muted">3 days ago</a>
                             <p class="mb-0 text-muted tx-12">1.3k New Products</p>
                         </li>
-                        <li class="mt-0"> <i
+                        <li class="mt-0"><i
                                 class="mdi mdi-cart-outline bg-danger-gradient text-white product-icon"></i> <span
                                 class="font-weight-semibold mb-4 tx-14 ">Total Sales</span> <a href="#"
-                                class="float-left tx-11 text-muted">35 mins ago</a>
+                                                                                               class="float-left tx-11 text-muted">35 mins ago</a>
                             <p class="mb-0 text-muted tx-12">1k New Sales</p>
                         </li>
-                        <li class="mt-0"> <i class="ti-bar-chart-alt bg-success-gradient text-white product-icon"></i>
+                        <li class="mt-0"><i class="ti-bar-chart-alt bg-success-gradient text-white product-icon"></i>
                             <span class="font-weight-semibold mb-4 tx-14 ">Toatal Revenue</span> <a href="#"
-                                class="float-left tx-11 text-muted">50 mins ago</a>
+                                                                                                    class="float-left tx-11 text-muted">50 mins ago</a>
                             <p class="mb-0 text-muted tx-12">23.5K New Revenue</p>
                         </li>
-                        <li class="mt-0"> <i class="ti-wallet bg-warning-gradient text-white product-icon"></i> <span
+                        <li class="mt-0"><i class="ti-wallet bg-warning-gradient text-white product-icon"></i> <span
                                 class="font-weight-semibold mb-4 tx-14 ">Toatal Profit</span> <a href="#"
-                                class="float-left tx-11 text-muted">1 hour ago</a>
+                                                                                                 class="float-left tx-11 text-muted">1 hour ago</a>
                             <p class="mb-0 text-muted tx-12">3k New profit</p>
                         </li>
-                        <li class="mt-0"> <i class="si si-eye bg-purple-gradient text-white product-icon"></i> <span
+                        <li class="mt-0"><i class="si si-eye bg-purple-gradient text-white product-icon"></i> <span
                                 class="font-weight-semibold mb-4 tx-14 ">Customer Visits</span> <a href="#"
-                                class="float-left tx-11 text-muted">1 day ago</a>
+                                                                                                   class="float-left tx-11 text-muted">1 day ago</a>
                             <p class="mb-0 text-muted tx-12">15% increased</p>
                         </li>
-                        <li class="mt-0 mb-0"> <i class="icon-note icons bg-primary-gradient text-white product-icon"></i>
+                        <li class="mt-0 mb-0"><i class="icon-note icons bg-primary-gradient text-white product-icon"></i>
                             <span class="font-weight-semibold mb-4 tx-14 ">Customer Reviews</span> <a href="#"
-                                class="float-left tx-11 text-muted">1 day ago</a>
+                                                                                                      class="float-left tx-11 text-muted">1 day ago</a>
                             <p class="mb-0 text-muted tx-12">1.5k reviews</p>
                         </li>
                     </ul>
@@ -358,7 +353,7 @@
                             <h4 class="font-weight-bold mb-2">$7,590</h4>
                             <div class="progress progress-style progress-sm">
                                 <div class="progress-bar bg-primary-gradient wd-80p" role="progressbar"
-                                    aria-valuenow="78" aria-valuemin="0" aria-valuemax="78"></div>
+                                     aria-valuenow="78" aria-valuemin="0" aria-valuemax="78"></div>
                             </div>
                         </div>
                         <div class="col-md-6 mt-4 mt-md-0">
@@ -368,7 +363,7 @@
                             <h4 class="font-weight-bold mb-2">$5,460</h4>
                             <div class="progress progress-style progress-sm">
                                 <div class="progress-bar bg-danger-gradient wd-75" role="progressbar" aria-valuenow="45"
-                                    aria-valuemin="0" aria-valuemax="45"></div>
+                                     aria-valuemin="0" aria-valuemax="45"></div>
                             </div>
                         </div>
                     </div>
@@ -422,44 +417,44 @@
                 <div class="table-responsive country-table">
                     <table class="table table-striped table-bordered mb-0 text-sm-nowrap text-lg-nowrap text-xl-nowrap">
                         <thead>
-                            <tr>
-                                <th class="wd-lg-25p">Date</th>
-                                <th class="wd-lg-25p tx-right">Sales Count</th>
-                                <th class="wd-lg-25p tx-right">Earnings</th>
-                                <th class="wd-lg-25p tx-right">Tax Witheld</th>
-                            </tr>
+                        <tr>
+                            <th class="wd-lg-25p">Date</th>
+                            <th class="wd-lg-25p tx-right">Sales Count</th>
+                            <th class="wd-lg-25p tx-right">Earnings</th>
+                            <th class="wd-lg-25p tx-right">Tax Witheld</th>
+                        </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>05 Dec 2019</td>
-                                <td class="tx-right tx-medium tx-inverse">34</td>
-                                <td class="tx-right tx-medium tx-inverse">$658.20</td>
-                                <td class="tx-right tx-medium tx-danger">-$45.10</td>
-                            </tr>
-                            <tr>
-                                <td>06 Dec 2019</td>
-                                <td class="tx-right tx-medium tx-inverse">26</td>
-                                <td class="tx-right tx-medium tx-inverse">$453.25</td>
-                                <td class="tx-right tx-medium tx-danger">-$15.02</td>
-                            </tr>
-                            <tr>
-                                <td>07 Dec 2019</td>
-                                <td class="tx-right tx-medium tx-inverse">34</td>
-                                <td class="tx-right tx-medium tx-inverse">$653.12</td>
-                                <td class="tx-right tx-medium tx-danger">-$13.45</td>
-                            </tr>
-                            <tr>
-                                <td>08 Dec 2019</td>
-                                <td class="tx-right tx-medium tx-inverse">45</td>
-                                <td class="tx-right tx-medium tx-inverse">$546.47</td>
-                                <td class="tx-right tx-medium tx-danger">-$24.22</td>
-                            </tr>
-                            <tr>
-                                <td>09 Dec 2019</td>
-                                <td class="tx-right tx-medium tx-inverse">31</td>
-                                <td class="tx-right tx-medium tx-inverse">$425.72</td>
-                                <td class="tx-right tx-medium tx-danger">-$25.01</td>
-                            </tr>
+                        <tr>
+                            <td>05 Dec 2019</td>
+                            <td class="tx-right tx-medium tx-inverse">34</td>
+                            <td class="tx-right tx-medium tx-inverse">$658.20</td>
+                            <td class="tx-right tx-medium tx-danger">-$45.10</td>
+                        </tr>
+                        <tr>
+                            <td>06 Dec 2019</td>
+                            <td class="tx-right tx-medium tx-inverse">26</td>
+                            <td class="tx-right tx-medium tx-inverse">$453.25</td>
+                            <td class="tx-right tx-medium tx-danger">-$15.02</td>
+                        </tr>
+                        <tr>
+                            <td>07 Dec 2019</td>
+                            <td class="tx-right tx-medium tx-inverse">34</td>
+                            <td class="tx-right tx-medium tx-inverse">$653.12</td>
+                            <td class="tx-right tx-medium tx-danger">-$13.45</td>
+                        </tr>
+                        <tr>
+                            <td>08 Dec 2019</td>
+                            <td class="tx-right tx-medium tx-inverse">45</td>
+                            <td class="tx-right tx-medium tx-inverse">$546.47</td>
+                            <td class="tx-right tx-medium tx-danger">-$24.22</td>
+                        </tr>
+                        <tr>
+                            <td>09 Dec 2019</td>
+                            <td class="tx-right tx-medium tx-inverse">31</td>
+                            <td class="tx-right tx-medium tx-inverse">$425.72</td>
+                            <td class="tx-right tx-medium tx-danger">-$25.01</td>
+                        </tr>
                         </tbody>
                     </table>
                 </div>
